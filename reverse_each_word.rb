9cf30b3a0780c7c2d02a6f7b22feba416f3da8ea
Michael Mottola <mottola64@gmail.com>
2019-02-27 00:59:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,3 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
-
-def reverse_each_word(string)
-  array = string.split(" ")
-  test_array = []
-  array.collect do|string|
-    reverse_array << string.reverse
-  end
-  reverse_array.join(" ")
-end
