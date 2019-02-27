24c19ba70b673af2ec68839875815477d2031d60
Michael Mottola <mottola64@gmail.com>
2019-02-27 00:59:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,17 @@
 def reverse_each_word(string)
   array = string.split(" ")
   return_array = []
-  array.each do |string|
+  array.collect do |string|
     return_array << string.reverse
   end
   return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string|
+    reverse_array << string.reverse
+  end
+  reverse_array.join(" ")
 end
